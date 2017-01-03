2fb5966a6fe87f54fea266279e79fc6ee030cdca
PapoteDetres <desertcoqui@yahoo.com>
2017-01-03 12:15:16 -0600

    First Java commit and push

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by PapoteDetres1 on 1/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Howdy people");
+        System.out.println("Que paso");
+        System.out.println("Goodbye, World");
+    }
+}
+
