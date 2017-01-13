98c018e13de82e35ef40984e5c2c364d74ad2963
PapoteDetres <desertcoqui@yahoo.com>
2017-01-12 19:26:46 -0600

    added a getString method in Validator.

--- src/TrafficApp.java
+++ src/TrafficApp.java
@@ -9,10 +9,11 @@ public class TrafficApp {
         Scanner scan = new Scanner(System.in);
         Validator validator = new Validator(scan);
 
-        String userChoice = validator.getString(scan, "Type in one of the following colors: BLUE, YELLOW or RED to find its function");
-        TrafficLightColor light= TrafficLightColor.valueOf(userChoice);
+        String userChoice = validator.getString(scan, "Type in one of the following colors in CAPS: RED, BLUE, YELLOW, WHITE, TEAL, BLACK, PURPLE, GREEN, MAGENTA, CYAN to find its function");
+        TrafficLightColor light = TrafficLightColor.valueOf(userChoice);
         WhatItDoes(light);
     }
+
     private static void WhatItDoes(TrafficLightColor light) {
         if (light.equals(TrafficLightColor.RED)) {
             System.out.println("You were suppose to STOP");
@@ -23,12 +24,32 @@ public class TrafficApp {
         } else if (light.equals(TrafficLightColor.YELLOW)) {
             System.out.println("This is a yellow light, slow it down!");
 
-        } else {
+
+        } else if (light.equals(TrafficLightColor.WHITE)) {
+            System.out.println("White is the color of chalk");
+
+        } else if (light.equals(TrafficLightColor.TEAL)) {
+            System.out.println("I once went to an all Teal party");
+
+        } else if (light.equals(TrafficLightColor.BLACK)) {
+            System.out.println("A blackhole is the color black from what we know");
+
+        } else if (light.equals(TrafficLightColor.PURPLE)) {
+            System.out.println("Remember that ugly Dinosaur? What color was he again");
+
+        } else if (light.equals(TrafficLightColor.GREEN)) {
+            System.out.println("What is the color of Marijuana");
+
+
+        } else if (light.equals(TrafficLightColor.MAGENTA)) {
+            System.out.println("I do not recall what the color magenta looks like");
+
+
+        } else if (light.equals(TrafficLightColor.CYAN)) {
+            System.out.println("Cyan is a screen printing color I believe");
+
 
         }
     }
+
 }
-
-
-
-
--- src/TrafficLightColor.java
+++ src/TrafficLightColor.java
@@ -2,7 +2,7 @@
  * Created by PapoteDetres1 on 1/12/17.
  */
 public enum TrafficLightColor {
-    BLUE,YELLOW,RED
+    BLUE,YELLOW,RED,WHITE,TEAL,BLACK,GREEN,MAGENTA,CYAN,PURPLE
     }
 
 
